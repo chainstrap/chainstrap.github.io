93385b4f85e1339ab954f2cd45c1ddf8b666957d
Chainstrap <chainstrap@example.com>
2021-04-28 04:18:22 +0000

    Add daily script

---
+++ scripts/daily_RVN.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Use:
+# sudo crontab -e
+# Add:
+# 4 0 * * * /root/chainstrap.github.io/scripts/daily_RVN.sh
+
+cd ~
+./savechain.sh RVN
+git add RVN/RVN-mainnet.json
+git commit -m 'Update chain'
+git push
+wget https://heartbeat.uptimerobot.com/m787981208-1a26bdea079ab569c84c73b62af23ebde7d9d081
