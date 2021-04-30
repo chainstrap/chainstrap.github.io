5c50d92b8dc5ed1ee793c725d46285a2db20095c
Chainstrap <chainstrap@example.com>
2021-04-30 19:33:18 +0000

    Restart daemon

--- scripts/daily_RVN.sh
+++ scripts/daily_RVN.sh
@@ -5,8 +5,10 @@
 # 4 0 * * * /root/chainstrap.github.io/scripts/daily_RVN.sh
 
 cd /root/chainstrap.github.io
-./savechain.py RVN
+./savechain.py RVN >> savechain.log
 git add RVN/RVN-mainnet.json
 git commit -m 'Update chain'
 git push
-wget https://heartbeat.uptimerobot.com/m787981208-1a26bdea079ab569c84c73b62af23ebde7d9d081
+wget --spider https://heartbeat.uptimerobot.com/m787981208-1a26bdea079ab569c84c73b62af23ebde7d9d081
+ravend -daemon
+
